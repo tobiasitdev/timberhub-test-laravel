1ee5eec7d1a61dfb3c98250389387291136ddc5a
tobiasitdev <tobias.ch8832@gmail.com>
2022-10-02 16:31:20 +0200

    add migration

---
+++ database/migrations/2022_10_02_101558_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('specy');
+            $table->string('grade');
+            $table->string('drying_method');
+            $table->string('treatment')->nullable();
+            $table->integer('thickness');
+            $table->integer('width');
+            $table->integer('height');
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
